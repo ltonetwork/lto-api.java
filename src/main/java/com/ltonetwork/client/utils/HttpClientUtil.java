d9d15a356ab1dac1a4a5c22f2ef029b4969c2f26
Kaloyan Tanev <tanevdev@gmail.com>
2021-06-14 16:31:09 +0300

    Add Post functions

--- src/main/java/com/ltonetwork/client/utils/HttpClientUtil.java
+++ src/main/java/com/ltonetwork/client/utils/HttpClientUtil.java
@@ -1,11 +1,14 @@
 package com.ltonetwork.client.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClientUtil {
 
@@ -16,17 +19,19 @@ public class HttpClientUtil {
                 .uri(uri)
                 .timeout(Duration.ofSeconds(30))
                 .header("Content-Type", "application/json")
+                .GET()
                 .build();
     }
 
-    public static void get(URI uri, HashMap<String, String> headers) {
+    public static void get(URI uri, Map<String, String> headers) {
         ArrayList<String> keys = new ArrayList<String>(headers.keySet());
         ArrayList<String> values = new ArrayList<String>(headers.values());
 
         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
                 .uri(uri)
                 .timeout(Duration.ofSeconds(30))
-                .header("Content-Type", "application/json");
+                .header("Content-Type", "application/json")
+                .GET();
 
         for(int i=0; i<keys.size(); i++){
             requestBuilder.header(keys.get(i), values.get(i));
@@ -35,4 +40,51 @@ public class HttpClientUtil {
         HttpRequest request = requestBuilder.build();
     }
 
+    public static void post(URI uri, Map<String,String> params) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = "";
+
+        try {
+            requestBody = objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(params);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .timeout(Duration.ofSeconds(30))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+    }
+
+    public static void post(URI uri, Map<String,String> params, Map<String,String> headers) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = "";
+
+        try {
+            requestBody = objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(params);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> keys = new ArrayList<String>(headers.keySet());
+        ArrayList<String> values = new ArrayList<String>(headers.values());
+
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                .uri(uri)
+                .timeout(Duration.ofSeconds(30))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody));
+
+        for (int i = 0; i < keys.size(); i++) {
+            requestBuilder.header(keys.get(i), values.get(i));
+        }
+
+        HttpRequest request = requestBuilder.build();
+    }
 }
