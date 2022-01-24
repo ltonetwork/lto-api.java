9e0d360cea44422aefdbfda66361ded3ae52d3ce
Kaloyan Tanev <tanevdev@gmail.com>
2022-01-24 13:30:55 +0100

    Add Association versions

--- src/main/java/com/ltonetwork/client/core/transaction/Association.java
+++ src/main/java/com/ltonetwork/client/core/transaction/Association.java
@@ -10,71 +10,96 @@ import com.ltonetwork.client.types.Encoding;
 import com.ltonetwork.client.types.JsonObject;
 import com.ltonetwork.client.utils.Encoder;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
 public class Association extends Transaction {
     private final static long MINIMUM_FEE = 100_000_000;
     private final static byte TYPE = 16;
-    private final static byte VERSION = 1;
+    private final static List<Byte> SUPPORTED_VERSIONS = Arrays.asList((byte) 1, (byte) 3);
     private final Address party;
     private final int associationType;
     private String hash;
+    private final long expires;
 
-    public Association(Address party, int type, String hash, Encoding encoding) {
-        super(TYPE, VERSION, MINIMUM_FEE);
+    public Association(Address party, int type, String hash, Encoding encoding, long expires, byte version) {
+        super(TYPE, version, MINIMUM_FEE);
+
+        if(!SUPPORTED_VERSIONS.contains(version))
+            throw new IllegalArgumentException("Unknown version, supported versions are: " + SUPPORTED_VERSIONS);
+        if(version == (byte) 1 && expires != 0)
+            throw new IllegalArgumentException("Association expiration is not supported on v1");
 
         this.party = party;
         this.associationType = type;
         this.hash = Encoder.base58Encode(Encoder.decode(hash, encoding));
+        this.expires = expires;
+    }
+
+    public Association(Address party, int type, String hash, Encoding encoding, byte version) {
+        this(party, type, hash, encoding, 0, version);
+    }
+
+    public Association(Address party, int type, String hash, Encoding encoding, long expires) {
+        this(party, type, hash, encoding, expires, (byte) 3);
+    }
+
+    public Association(Address party, int type, String hash, Encoding encoding) {
+        this(party, type, hash, encoding, 0, (byte) 3);
+    }
+
+    public Association(Address party, int type, long expires, byte version) {
+        super(TYPE, version, MINIMUM_FEE);
+
+        if(!SUPPORTED_VERSIONS.contains(version))
+            throw new IllegalArgumentException("Unknown version, supported versions are: " + SUPPORTED_VERSIONS);
+        if(version == (byte) 1 && expires != 0)
+            throw new IllegalArgumentException("Association expiration is not supported on v1");
+
+        this.party = party;
+        this.associationType = type;
+        this.expires = expires;
+    }
+
+    public Association(Address party, int type, byte version) {
+        this(party, type, 0, version);
+    }
+
+    public Association(Address party, int type, long expires) {
+        this(party, type, expires, (byte) 3);
     }
 
     public Association(Address party, int type) {
-        super(TYPE, VERSION, MINIMUM_FEE);
-        this.party = party;
-        this.associationType = type;
+        this(party, type, 0, (byte) 3);
     }
 
     public Association(JsonObject json) {
         super(json);
+
+        byte versionFromJson = Byte.parseByte(json.get("version").toString());
+        long expiresFromJson = json.has("expires") ? Long.parseLong(json.get("expires").toString()) : 0;
+
+        if(!SUPPORTED_VERSIONS.contains(versionFromJson))
+            throw new IllegalArgumentException("Unknown version, supported versions are: " + SUPPORTED_VERSIONS);
+        if(versionFromJson == (byte) 1 && expiresFromJson != 0)
+            throw new IllegalArgumentException("Association expiration is not supported on v1");
+
         this.party = new Address(json.get("party").toString());
         this.associationType = Integer.parseInt(json.get("associationType").toString());
+        this.expires = expiresFromJson;
         if (json.has("hash")) this.hash = json.get("hash").toString();
     }
 
     public byte[] toBinary() {
-        if (this.senderPublicKey == null) {
-            throw new BadMethodCallException("Sender public key not set");
+        if (this.senderPublicKey == null) throw new BadMethodCallException("Sender public key not set");
+        if (this.timestamp == 0) throw new BadMethodCallException("Timestamp not set");
+
+        switch(version) {
+            case (byte) 1: return toBinaryV1();
+            case (byte) 3: return toBinaryV3();
+            default: throw new IllegalArgumentException("Unknown version " + version);
         }
-
-        if (this.timestamp == 0) {
-            throw new BadMethodCallException("Timestamp not set");
-        }
-
-        byte[] ret = Bytes.concat(
-                new byte[]{this.type},
-                new byte[]{this.version},
-                new byte[]{this.getNetwork()},
-                this.senderPublicKey.getRaw(),
-                Encoder.base58Decode(this.party.getAddress()),
-                Ints.toByteArray(associationType)
-        );
-
-        if (hash != null) {
-            byte[] rawHash = Encoder.base58Decode(this.hash);
-            ret = Bytes.concat(
-                    ret,
-                    new byte[]{(byte) 1},
-                    Shorts.toByteArray((short) rawHash.length),
-                    rawHash);
-        } else {
-            ret = Bytes.concat(
-                    ret,
-                    new byte[]{(byte) 0});
-        }
-
-        return Bytes.concat(
-                ret,
-                Longs.toByteArray(this.timestamp),
-                Longs.toByteArray(this.fee)
-        );
     }
 
     public String getHash(Encoding encoding) {
@@ -88,5 +113,50 @@ public class Association extends Transaction {
 
     public String getHash() {
         return getHash(Encoding.BASE58);
+    }
+
+    public long getExpiration() {
+        return this.expires;
+    }
+
+    private byte[] toBinaryV1(){
+        return Bytes.concat(
+                new byte[]{this.type},                          // 1b
+                new byte[]{this.version},                       // 1b
+                new byte[]{this.getNetwork()},                  // 1b
+                this.senderPublicKey.getRaw(),                  // 32b
+                Encoder.base58Decode(this.party.getAddress()),  // 26b
+                Ints.toByteArray(associationType),              // 4b
+                hashToBinary(),                                 // 1b + (2b + nb)
+                Longs.toByteArray(this.timestamp),              // 8b
+                Longs.toByteArray(this.fee)                     // 8b
+        );
+    }
+
+    private byte[] toBinaryV3(){
+        return Bytes.concat(
+                new byte[]{this.type},                          // 1b
+                new byte[]{this.version},                       // 1b
+                new byte[]{this.getNetwork()},                  // 1b
+                Longs.toByteArray(this.timestamp),              // 8b
+                this.senderPublicKey.toBinary(),                // 33b/34b
+                Longs.toByteArray(this.fee),                    // 8b
+                Encoder.base58Decode(this.party.getAddress()),  // 26b
+                Ints.toByteArray(associationType),              // 4b
+                Longs.toByteArray(expires),                     // 8b
+                hashToBinary()                                  // 1b + (2b + nb)
+        );
+    }
+
+    private byte[] hashToBinary(){
+        if (hash != null) {
+            byte[] rawHash = Encoder.base58Decode(this.hash);
+            return Bytes.concat(
+                    new byte[]{(byte) 1},
+                    Shorts.toByteArray((short) rawHash.length),
+                    rawHash);
+        } else {
+            return new byte[]{(byte) 0};
+        }
     }
 }
--- src/test/java/com/ltonetwork/client/core/transaction/AssociationTest.java
+++ src/test/java/com/ltonetwork/client/core/transaction/AssociationTest.java
@@ -25,11 +25,31 @@ public class AssociationTest {
     }
 
     @Test
-    public void testToBinary() {
+    public void testToBinaryV3() {
         Account account = TestUtil.createAccount();
         tx.signWith(account);
 
-        assertEquals(88, tx.toBinary().length);
+        assertEquals(97, tx.toBinary().length);
+    }
+
+    @Test
+    public void testToBinaryV3WithExpiry() {
+        long expiration = 12345678;
+        Account account = TestUtil.createAccount();
+        Association txWithExpiry = new Association(new Address("3MsE8Jfjkh2zaZ1LCGqaDzB5nAYw5FXhfCx"), 1, "hash", Encoding.RAW, expiration);
+        txWithExpiry.signWith(account);
+
+        assertEquals(97, txWithExpiry.toBinary().length);
+        assertEquals(expiration, txWithExpiry.getExpiration());
+    }
+
+    @Test
+    public void testToBinaryV1() {
+        Association txV1 = new Association(new Address("3MsE8Jfjkh2zaZ1LCGqaDzB5nAYw5FXhfCx"), 1, "hash", Encoding.RAW, (byte) 1);
+        Account account = TestUtil.createAccount();
+        txV1.signWith(account);
+
+        assertEquals(88, txV1.toBinary().length);
     }
 
     @Test
@@ -55,13 +75,13 @@ public class AssociationTest {
         Association txNoHash = new Association(new Address("3MsE8Jfjkh2zaZ1LCGqaDzB5nAYw5FXhfCx"), 1);
         txNoHash.signWith(account);
 
-        assertEquals(82, txNoHash.toBinary().length);
+        assertEquals(91, txNoHash.toBinary().length);
 
         txNoHash.getHash();
     }
 
     @Test
-    public void testCreateWithJson() {
+    public void testCreateWithJsonV1() {
         JsonObject json = new JsonObject(
                 "{\n" +
                         "  \"type\": 16,\n" +
@@ -81,6 +101,30 @@ public class AssociationTest {
 
         Association jsonTx = new Association(json);
         assertEquals(88, jsonTx.toBinary().length);
+    }
+
+    @Test
+    public void testCreateWithJsonV3() {
+        JsonObject json = new JsonObject(
+                "{\n" +
+                        "  \"type\": 16,\n" +
+                        "  \"id\": \"oYv8LBTsLRyAq1w7n9UXudAf5Luu9CuRXkYSnxLX2oa\",\n" +
+                        "  \"sender\": \"3N51gbw5W3xvSkcAXtLnXc3SQh2m9e6TBcy\",\n" +
+                        "  \"senderPublicKey\": \"8wFR3b8WnbFaxQEdRnogTqC5doYUrotm3P7upvxPaWUo\",\n" +
+                        "  \"fee\": 100000,\n" +
+                        "  \"timestamp\": 1538728794530,\n" +
+                        "  \"proofs\": [\"65E82MLn6RdF7Y2VrdtFWkHd97teqLSwVdbGyEfy7x6aczkHRDZMvNUfdTAYgqDXzDDKKEkQqVhMVMg6EEEvE3C3\"],\n" +
+                        "  \"version\": 3,\n" +
+                        "  \"recipient\": \"3Mv7ajrPLKewkBNqfxwRZoRwW6fziehp7dQ\",\n" +
+                        "  \"party\": \"3N51gbw5W3xvSkcAXtLnXc3SQh2m9e6TBcy\",\n" +
+                        "  \"associationType\": \"1\",\n" +
+                        "  \"hash\": \"3fkSoZ\",\n" + //base58 of "hash"
+                        "  \"expires\": \"12345678\",\n" +
+                        "  \"height\": 22654\n" +
+                        "}", false);
+
+        Association jsonTx = new Association(json);
+        assertEquals(97, jsonTx.toBinary().length);
     }
 
     @Test
